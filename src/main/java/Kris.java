b03458b571a48cb943bf24544ef99675999e7100
fql123 <fengqilong293@gmail.com>
2025-08-20 17:14:23 +0800

    Completed Level 0

---
+++ src/main/java/Kris.java
@@ -0,0 +1,17 @@
+public class Kris {
+    public static void main(String[] args) {
+        String logo = " _  __     _     \n"
+                + "| |/ /_ __(_)___ \n"
+                + "| ' /| '__| / __|\n"
+                + "| . \\| |  | \\__ \\\n"
+                + "|_|\\_\\_|  |_|___/\n";
+        System.out.println("Hello from\n" + logo);
+        
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Kris");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
