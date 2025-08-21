ecaf8f4b90efa061fecccdd30ef4b0590cec8abe
fql123 <fengqilong293@gmail.com>
2025-08-21 21:07:04 +0700

    Done with Level-1

--- src/main/java/Kris.java
+++ src/main/java/Kris.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Kris {
     public static void main(String[] args) {
         String logo = " _  __     _     \n"
@@ -8,10 +10,29 @@ public class Kris {
         System.out.println("Hello from\n" + logo);
         
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Kris");
-        System.out.println(" What can I do for you?");
+        System.out.println(" Yo yo yo! I'm Kris, your rap bot extraordinaire!");
+        System.out.println(" Drop me some beats... I mean commands! What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        
+        while (true) {
+            input = scanner.nextLine();
+            
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Peace out! Keep it real, catch you on the flip side!");
+                System.out.println(" Hope to see you again soon, my friend!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" " + input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        
+        scanner.close();
     }
 }
